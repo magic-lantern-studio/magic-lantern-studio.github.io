711dc9acb225b70d9b7991713df9b8ffce6702ca
Mark Millard <msm@wizzerworks.com>
2025-05-20 14:52:39 -0600

    Adding SoGtk API documentation.

---
+++ doc/sogtk/html/files_dup.js
@@ -0,0 +1,19 @@
+var files_dup =
+[
+    [ "SoGtk.h", "SoGtk_8h_source.html", null ],
+    [ "SoGtkBasic.h", "SoGtkBasic_8h_source.html", null ],
+    [ "SoGtkComponent.h", "SoGtkComponent_8h_source.html", null ],
+    [ "SoGtkConstrainedViewer.h", "SoGtkConstrainedViewer_8h_source.html", null ],
+    [ "SoGtkCursor.h", "SoGtkCursor_8h_source.html", null ],
+    [ "SoGtkDevice.h", "SoGtkDevice_8h_source.html", null ],
+    [ "SoGtkExaminerViewer.h", "SoGtkExaminerViewer_8h_source.html", null ],
+    [ "SoGtkFlyViewer.h", "SoGtkFlyViewer_8h_source.html", null ],
+    [ "SoGtkFullViewer.h", "SoGtkFullViewer_8h_source.html", null ],
+    [ "SoGtkGLWidget.h", "SoGtkGLWidget_8h_source.html", null ],
+    [ "SoGtkKeyboard.h", "SoGtkKeyboard_8h_source.html", null ],
+    [ "SoGtkMouse.h", "SoGtkMouse_8h_source.html", null ],
+    [ "SoGtkObject.h", "SoGtkObject_8h_source.html", null ],
+    [ "SoGtkPlaneViewer.h", "SoGtkPlaneViewer_8h_source.html", null ],
+    [ "SoGtkRenderArea.h", "SoGtkRenderArea_8h_source.html", null ],
+    [ "SoGtkViewer.h", "SoGtkViewer_8h_source.html", null ]
+];
